fcad6c9511af618208f8edd5822416731bcc575f
Muhammad Hassan Raza <mhassanraza117@gmail.com>
2026-01-21 14:06:56 +0500

    profixes

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,10 @@ export default defineConfig(({ mode }) => ({
         proxyTimeout: 60000,
         autoRewrite: true,
         cookieDomainRewrite: "",
+        headers: {
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+          "Connection": "keep-alive"
+        },
         rewrite: (path) => path.replace(/^\/api\/uaf/, ""),
         configure: (proxy, options) => {
           proxy.on("proxyRes", (proxyRes, req, res) => {
